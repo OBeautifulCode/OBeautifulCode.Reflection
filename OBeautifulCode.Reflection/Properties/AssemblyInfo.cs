cd7111dc4930eeb96d58d86b4c9877ac0463fbf9
Suraj Gupta <surajg@gmail.com>
2016-06-06 19:38:34 -0400

    add appveyor YML as solution item fix up gitignore/gitattributes add nuspec file and icon image fix AssemblyInfo.cs

--- OBeautifulCode.Reflection/Properties/AssemblyInfo.cs
+++ OBeautifulCode.Reflection/Properties/AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OBeautifulCode.Reflection")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OBeautifulCode.Reflection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.Reflection")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016 OBeautifulCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
