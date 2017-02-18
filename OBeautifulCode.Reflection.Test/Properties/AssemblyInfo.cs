0f5361a08cfa8ea11e7f731aeb70309b6a933928
Suraj K. Gupta <surajg@gmail.com>
2017-02-18 10:22:29 -0500

    cleanup

--- OBeautifulCode.Reflection.Test/Properties/AssemblyInfo.cs
+++ OBeautifulCode.Reflection.Test/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.Reflection.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2016 OBeautifulCode")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 OBeautifulCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- OBeautifulCode.Reflection/Properties/AssemblyInfo.cs
+++ OBeautifulCode.Reflection/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.Reflection")]
-[assembly: AssemblyCopyright("Copyright ©  2016 OBeautifulCode")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 OBeautifulCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
